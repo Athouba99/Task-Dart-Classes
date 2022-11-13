113591a4f6b33b4e34f589150910b7f0576da961
athouba99 <athooba99@gmail.com>
2022-11-13 18:57:59 +0300

    Dart task6 (classes is done)

--- main.dart
+++ main.dart
@@ -1,8 +1,21 @@
+class Movie {
+  String name;
+  String genre;
+  int ratings;
+
+  Movie({required this.name, required this.genre, required this.ratings}) {}
+
+  void printDetails() {
+    print(
+        " The movie name is ${name},\n the movie genre is ${genre},\n the movie rating is ${ratings}");
+  }
+}
+
 void main() {
   final movie = {
     'name': 'The Amazing SpiderMan',
     'genre': 'Action',
-    'ratings': [1.5,3.2,6.4]
+    'ratings': [1.5, 3.2, 6.4]
   };
   print(movie['name']);
   print(movie['genre']);
